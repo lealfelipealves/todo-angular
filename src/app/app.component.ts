ca645142e83c0bf2d781dacd110b7cd27b0c3151
Felipe Leal <lealfelipealves@gmail.com>
2021-10-23 00:35:43 -0300

    chore: update ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent {
 
   constructor() {
     this.todos.push(new Todo(1, 'Passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', true));
     this.todos.push(new Todo(3, 'Cortar cabelo', false));
   }
 }
