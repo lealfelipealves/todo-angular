4d08faaa9119a8145ed05e7d900d304f2e4180f6
Felipe Leal <lealfelipealves@gmail.com>
2021-10-23 00:45:23 -0300

    chore: implemented remove, markAsDone, markAsUndone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,4 +15,19 @@ export class AppComponent {
     this.todos.push(new Todo(2, 'Ir ao supermercado', true));
     this.todos.push(new Todo(3, 'Cortar cabelo', false));
   }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo) {
+    todo.done = false;
+  }
 }
